f2582981567f82e3331029e0dd534be3aa3a1092
liyiysng <liyiysng@163.com>
2021-01-09 18:59:50 +0800

    metrics

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"math/rand"
+	"time"
 
-	"github.com/liyiysng/scatter/node"
+	"github.com/liyiysng/scatter/metrics"
+
+	"log"
 )
 
 func main() {
-	fmt.Printf("hello world%s\n", "!!!")
-	n := &node.Node{Name: "foo node"}
-	fmt.Println(n)
+
+	go func() {
+
+		method := []string{"foo", "bar"}
+		endpoint := []string{"endpoint:8080", "endpoint:9090"}
+
+		for {
+			m := method[rand.Int31n(int32(len(method)))]
+			e := method[rand.Int31n(int32(len(endpoint)))]
+			log.Printf("sent %s %s\n", m, e)
+			metrics.UserInput(m, e)
+			time.Sleep(time.Millisecond * time.Duration(rand.Int31n(2000)))
+		}
+	}()
+
+	err := metrics.StartListen(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
