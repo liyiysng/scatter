850135524026ceb91fc16d7176dcf3ec3c562850
liyiysng <liyiysng@163.com>
2021-01-12 14:53:01 +0800

    move cmd to tool dir

--- cmd/gen_proto.bat
+++
@@ -1,2 +0,0 @@
-protoc --go_out=.. --proto_path=../proto ../proto/node/node.proto
-protoc --go-grpc_out=.. --proto_path=../proto ../proto/node/node.proto
---
+++ tool/cmd/gen_proto.bat
@@ -0,0 +1,2 @@
+protoc --go_out=../.. --proto_path=../../proto ../../proto/node/node.proto
+protoc --go-grpc_out=../.. --proto_path=../../proto ../../proto/node/node.proto
