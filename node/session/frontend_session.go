43ae0b68f66c26d4c2b8d0f01baba3969c3e27d1
liyiysng <liyiysng@163.com>
2021-01-29 18:42:42 +0800

    session

--- node/session/frontend_session.go
+++ node/session/frontend_session.go
@@ -1,6 +1,11 @@
 package session
 
-import "github.com/liyiysng/scatter/node/conn"
+import (
+	"context"
+
+	"github.com/liyiysng/scatter/node/conn"
+	"google.golang.org/protobuf/proto"
+)
 
 type frontendSession struct {
 	sid  string
@@ -9,8 +14,12 @@ type frontendSession struct {
 }
 
 // NewFrontendSession 创建一个session
-func NewFrontendSession(c conn.MsgConn) {
-
+func NewFrontendSession(nid string, c conn.MsgConn) Session {
+	return &frontendSession{
+		sid:  "",
+		nid:  nid,
+		conn: c,
+	}
 }
 
 func (s *frontendSession) Stats() State {
@@ -19,4 +28,16 @@ func (s *frontendSession) Stats() State {
 		NID:           s.nid,
 		RemoteAddress: s.conn.RemoteAddr().String(),
 	}
+}
+
+func (s *frontendSession) Push(ctx context.Context, cmd string, msg proto.Message) error {
+	panic("unimplement")
+}
+
+func (s *frontendSession) OnClose(onClose OnClose) {
+	panic("unimplement")
+}
+
+func (s *frontendSession) Closed() bool {
+	panic("unimplement")
 }
